e0fa1f4a5121fa6ff756ad46b04b77af359682d8
pythonshadow404 <pythonshadow404@gmail.com>
2026-05-21 16:03:03 -0500

    Add 2_sales_prediction

---
+++ Ch2_input_output/2_sales_prediction.py
@@ -0,0 +1,6 @@
+#Prompt user to input total sales amount
+total_sales = input("Enter projected total sales amount: ")
+#Calculate the profit of 23% of total sales
+profit = float(total_sales) * 0.23
+#Display the profit
+print("Projected Profit:", profit)
