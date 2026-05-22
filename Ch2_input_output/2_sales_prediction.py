c3c9ab5d334bb8044f295ab95c72eefd6a47e7f6
pythonshadow404 <pythonshadow404@gmail.com>
2026-05-22 13:43:09 -0500

    Modify 2_sales_prediction with decimal place formatting

--- Ch2_input_output/2_sales_prediction.py
+++ Ch2_input_output/2_sales_prediction.py
@@ -2,5 +2,6 @@
 total_sales = input("Enter projected total sales amount: ")
 #Calculate the profit of 23% of total sales
 profit = float(total_sales) * 0.23
-#Display the profit
-print("Projected Profit:", profit)
+#Display the profit with two decimal places 
+# --format(12345.2578, '.2f')
+print("Projected Profit: $", format(profit, '.2f'), sep='')
