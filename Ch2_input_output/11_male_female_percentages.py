b97038eef44af1c6188fe2a1f2250e1d299fc579
pythonshadow404 <pythonshadow404@gmail.com>
2026-05-25 23:43:18 -0500

    Add questions 9, 10, 11

---
+++ Ch2_input_output/11_male_female_percentages.py
@@ -0,0 +1,5 @@
+males = int(input("Enter the number of male students: "))
+females = int(input("Enter the number of female students: "))
+total_students = males + females
+print("Percentage of male students:", (males/total_students)*100)
+print("Percentage of male students:", (females/total_students)*100)
