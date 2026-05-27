a7ec3cec5cbc9ae524735eed6d0e8823c4464e0f
pythonshadow404 <pythonshadow404@gmail.com>
2026-05-27 14:58:27 -0500

    Add question 12_investment_transaction

--- Ch2_input_output/12_stock_transaction.py
+++ Ch2_input_output/12_stock_transaction.py
@@ -1,4 +1,13 @@
 #how much Joe paid for the stock
 buy_price = float(input("Enter the stock buy price:"))
+sell_price = float(input("Enter the stock sell price:"))
+#principle
+shares = 2000
 #commission
 commission = 0.03
+initial_principle_commission = (shares*commission) * buy_price
+final_principle_commission = (shares*commission) * sell_price
+
+final_investment = final_principle_commission - initial_principle_commission
+print(final_investment)
+
