934d2bb3e8b8696ec1bb339cf07ec16a88f80eb3
pythonshadow404 <pythonshadow404@gmail.com>
2026-05-29 16:24:44 -0500

    Modify question 14_compound_interest

--- Ch2_input_output/14_compound_interest.py
+++ Ch2_input_output/14_compound_interest.py
@@ -1,2 +1,6 @@
 # 14_compound_interest
 principle = float(input("Enter the principle amount: "))
+interest = float(input("Enter the interest amount: "))
+years = float(input("Enter the number of years: "))
+compounded_series = float(input("Enter the compounded series: "))
+
