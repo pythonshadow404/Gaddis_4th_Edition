cfb08fdf9a9e29504cdb3cd9cbbc064d273d6e6d
pythonshadow404 <pythonshadow404@gmail.com>
2026-06-08 15:50:22 -0500

    Add 4. Roman Numerals

--- Ch3_decision_structures/4_roman_numerals.py
+++ Ch3_decision_structures/4_roman_numerals.py
@@ -1,1 +1,24 @@
 #4. Roman Numerals
+number = int(input("Enter a number from 1 to 10: "))
+if number == 1: 
+    print("I")
+elif number == 2: 
+    print("II")
+elif number == 3: 
+    print("III")
+elif number == 4: 
+    print("IV")
+elif number == 5: 
+    print("V")
+elif number == 6: 
+    print("VI")
+elif number == 7: 
+    print("VII")
+elif number == 8: 
+    print("VII")
+elif number == 9: 
+    print("IX")
+elif number == 10: 
+    print("X")
+else: 
+    print("Enter a number from 1 to 10")
