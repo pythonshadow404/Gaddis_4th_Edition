e549732b61bdbdd55acd02283882dfd2866063ce
pythonshadow404 <pythonshadow404@gmail.com>
2026-06-14 14:29:32 -0500

    Modify 7. Color Mixer

--- Ch3_decision_structures/7_color_mixer.py
+++ Ch3_decision_structures/7_color_mixer.py
@@ -4,3 +4,9 @@ primary_color2 = input("Please enter another primary color: blue, red, or green:
 
 if primary_color1 == "red" and primary_color2 == "blue":
     print("Red and blue give you purple")
+elif primary_color1 == "red" and primary_color2 == "yellow":
+    print("Red and blue give you orange")
+elif primary_color1 == "blue" and primary_color2 == "yellow":
+    print("Yellow and blue give you get green")
+else: 
+    print("Please enter a primary color: blue, red, or yellow")
