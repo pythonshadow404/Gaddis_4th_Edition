1708a9c5e52542ad5ca587c95da30ff9c16eee31
pythonshadow404 <pythonshadow404@gmail.com>
2026-06-16 21:45:38 -0500

    Modify 8. Hot Dog Cookout Calculator

--- Ch3_decision_structures/8_hot_dog_cookout_calculator.py
+++ Ch3_decision_structures/8_hot_dog_cookout_calculator.py
@@ -1,3 +1,7 @@
 # 8.Hot Dog Cookout Calculator
+HOT_DOG_PACKAGE = 10
+BUN_PACKAGE = 8
 guests = int(input("Enter the number guest attending event: "))
 dogs_per_person = int(input("Enter the number hot dogs for each guest:  "))
+packages_needed = (guests * dogs_per_person) / HOT_DOG_PACKAGE
+print("Number of packages: ", packages_needed)
