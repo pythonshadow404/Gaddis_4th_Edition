d2d56d29ad5d949266d34af7fb5377989002bc73
pythonshadow404 <pythonshadow404@gmail.com>
2026-06-29 22:42:27 -0500

    Modify 9. Roulette Wheel Colors

--- Ch3_decision_structures/9_roulette_wheel_colors.py
+++ Ch3_decision_structures/9_roulette_wheel_colors.py
@@ -38,4 +38,92 @@ if pocket_number >= 1 and pocket_number <= 10:
         pocket_10 = "black"
         print("Pocket 10 is", pocket_10)
     
+if pocket_number >= 11 and pocket_number <= 18:
+    if pocket_number == 11:
+        pocket_11 = "black"
+        print("Pocket 11 is", pocket_11)
+    elif pocket_number == 12:
+        pocket_12 = "red" 
+        print("Pocket 12 is", pocket_12)
+    elif pocket_number == 13:
+        pocket_13 = "black" 
+        print("Pocket 13 is", pocket_13)
+    elif pocket_number == 14:
+        pocket_14 = "red" 
+        print("Pocket 14 is", pocket_14)
+    elif pocket_number == 15:
+        pocket_15 = "black"
+        print("Pocket 15 is", pocket_15) 
+    elif pocket_number == 16:
+        pocket_16 = "red" 
+        print("Pocket 16 is", pocket_16)
+    elif pocket_number == 17:
+        pocket_17 = "black" 
+        print("Pocket 17 is", pocket_17)
+    else:
+        pocket_18 = "red"
+        print("Pocket 18 is", pocket_18)
 
+if pocket_number >= 19 and pocket_number <= 28:
+    if pocket_number == 19:
+        pocket_19 = "red"
+        print("Pocket 19 is", pocket_19)
+    elif pocket_number == 20:
+        pocket_20 = "black" 
+        print("Pocket 20 is", pocket_20)
+    elif pocket_number == 21:
+        pocket_21 = "red" 
+        print("Pocket 21 is", pocket_21)
+    elif pocket_number == 22:
+        pocket_22 = "black" 
+        print("Pocket 22 is", pocket_22)
+    elif pocket_number == 23:
+        pocket_23 = "red"
+        print("Pocket 23 is", pocket_23) 
+    elif pocket_number == 24:
+        pocket_24 = "black" 
+        print("Pocket 24 is", pocket_24)
+    elif pocket_number == 25:
+        pocket_25 = "red" 
+        print("Pocket 25 is", pocket_25)
+    elif pocket_number == 26:
+        pocket_26 = "black" 
+        print("Pocket 26 is", pocket_26)
+    elif pocket_number == 27:
+        pocket_27 = "red" 
+        print("Pocket 27 is", pocket_27)
+    else:
+        pocket_28 = "black"
+        print("Pocket 28 is", pocket_28)
+
+if pocket_number >= 29 and pocket_number <= 36:
+    if pocket_number == 29:
+        pocket_29 = "black"
+        print("Pocket 29 is", pocket_29)
+    elif pocket_number == 20:
+        pocket_20 = "red" 
+        print("Pocket 20 is", pocket_20)
+    elif pocket_number == 21:
+        pocket_21 = "black" 
+        print("Pocket 21 is", pocket_21)
+    elif pocket_number == 22:
+        pocket_22 = "red" 
+        print("Pocket 22 is", pocket_22)
+    elif pocket_number == 23:
+        pocket_23 = "black"
+        print("Pocket 23 is", pocket_23) 
+    elif pocket_number == 24:
+        pocket_24 = "red" 
+        print("Pocket 24 is", pocket_24)
+    elif pocket_number == 25:
+        pocket_25 = "black" 
+        print("Pocket 25 is", pocket_25)
+    elif pocket_number == 26:
+        pocket_26 = "red" 
+        print("Pocket 26 is", pocket_26)
+    elif pocket_number == 35:
+        pocket_35 = "black" 
+        print("Pocket 35 is", pocket_35)
+    else:
+        pocket_36 = "red"
+        print("Pocket 36 is", pocket_36)
